5afe43b2b3f813a0b47cf63da4723258967e75a2
mashalla <aegis@iao.fraunhofer.de>
2011-11-23 10:38:04 +0100

    Header fixed

--- Source/accordion.js
+++ Source/accordion.js
@@ -1,15 +1,15 @@
 /*
- ---
- script: Fx.Accordion.js
- description: An Fx.Elements extension which allows you to easily create accordion type controls.
- license: MIT-style license
- authors:
- - Eva Lösch
- - Valerio Proietti
- requires:
- - core:1.2.4/Element.Event
- - /Fx.Elements
- provides: [Fx.Accordion]
+---
+script: Fx.Accordion.js
+description: An Fx.Elements extension which allows you to easily create accordion type controls.
+license: MIT-style license
+authors:
+- Christian Merz
+- Valerio Proietti
+requires:
+- core:1.2.4/Element.Event
+- /Fx.Elements
+provides: [Fx.Accordion]
  ...
  */
 Fx.Accordion = Class.refactor(Fx.Accordion, {
